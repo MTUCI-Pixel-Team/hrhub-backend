d6aa73c97d84a1f577627730128c4c02dd96dddc
Dizi <plastinin.denis2009@mail.ru>
2024-07-01 01:45:47 +0300

    improvement of the message and user model

--- backend/messaging_app/serializers.py
+++ backend/messaging_app/serializers.py
@@ -4,14 +4,25 @@ from services_app.models import ServiceAccount
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    account_id = serializers.IntegerField(source='account.id')
+    account_id = serializers.IntegerField(source='account.id', read_only=True)
+    username = serializers.SlugRelatedField(source='account.user_id', slug_field='username', read_only=True)
 
     class Meta:
         model = Message
-        fields = ['id', 'account_id', 'from_username', 'from_userphone', 'text', 'received_at']
+        fields = ['id', 'account_id', 'username', 'from_username', 'from_userphone', 'text', 'personal_chat_link',
+                  'received_at', 'is_read']
         read_only_fields = ['account']
 
     def create(self, validated_data):
-        account_id = validated_data.pop('account_id')
-        account = ServiceAccount.objects.get(id=account_id)
+        request = self.context.get('request')
+        account = ServiceAccount.objects.get(user_id=request.user.id)
         return Message.objects.create(account=account, **validated_data)
+
+
+class MessageUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'account', 'from_username', 'from_userphone', 'text', 'personal_chat_link', 'received_at',
+                  'is_read']
+        read_only_fields = ['id', 'account', 'from_username', 'from_userphone', 'text', 'personal_chat_link',
+                            'received_at']
--- backend/messaging_app/urls.py
+++ backend/messaging_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('message/create/', views.MessageCreateView.as_view(), name='Message_create'),
     path('message/list/', views.MessageListView.as_view(), name='Message_list'),
+    path('message/delete/<int:id>/', views.MessageDeleteView.as_view(), name='Message_delete'),
+    path('message/update/<int:id>/', views.MessageUpdateView.as_view(), name='Message_update'),
+    path('message/unread/', views.UnreadMessageListView.as_view(), name='Message_unread'),
 ]
