cbafe5f8a5d973b81cd1bfb6af9cb64a0867d455
Dizi <plastinin.denis2009@mail.ru>
2024-07-03 03:44:10 +0300

    simple fix

--- backend/tg_bot/app/keyboards.py
+++ backend/tg_bot/app/keyboards.py
@@ -5,7 +5,7 @@ import aiohttp
 # Изменение в get_specialists_keyboard
 async def get_specialists_keyboard():
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://147.45.40.23:7000/api/services/serviceaccount/telegram/') as response:
+        async with session.get('http://147.45.40.23:7000/api/service/list_tg/') as response:
             if response.status == 200:
                 specialists = await response.json()
                 keyboard = [[KeyboardButton(text=f"{specialist['service_username']} (ID: {specialist['id']})")] for
