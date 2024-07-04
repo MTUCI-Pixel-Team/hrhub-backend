0c53e1648dc2d793103cd79e8059656e6827f45f
defskela <sashokafanas@gmail.com>
2024-07-04 13:08:36 +0300

    Переделать main под асинхронное программирование

--- backend/parserMailYandex/main.py
+++ backend/parserMailYandex/main.py
@@ -1,25 +1,24 @@
 """
-    Файл отвечает за чтение входящих писем с почты HR-a
+    Файл отвечает за чтение входящих писем на почту HR-a
 """
 import re
 from time import sleep
 
-from read_incoming_emails import read_incoming_emails
+from app.api_utils import get_hr_list, read_incoming_emails
 
+# Паттерн для проверки почты на соответствие почте yandex.ru
 PATTERN_YANDEX_EMAIL = r'^[a-zA-Z0-9._%+-]+@yandex\.ru$'
 
-HR_EMAILS = ['jiopydryk011@yandex.ru', '1@yandex.ru']
-# Пароль приложений. Создается в специальном разделе
-HR_PASSWORDS = ['tdigxyrpiliyyyqq', '2']
-
-assert len(HR_PASSWORDS) == len(HR_EMAILS), 'Количество паролей не совпадает с количеством почтовых ящиков'
 while True:
-    for i in range(min(len(HR_EMAILS), len(HR_PASSWORDS))):
+    HR_EMAILS, HR_PASSWORDS, HR_IDS = get_hr_list()
+    print(f'Emails: {HR_EMAILS},\nPasswords: {HR_PASSWORDS}\n,IDS: {HR_IDS}')
+    for index, email in enumerate(HR_EMAILS):
         try:
-            if re.match(PATTERN_YANDEX_EMAIL, HR_EMAILS[i]) is None:
+            if re.match(PATTERN_YANDEX_EMAIL, email) is None:
                 raise ValueError('Неверный формат почты')
         except ValueError as e:
-            print(e, f'Название почты: {HR_EMAILS[i]}')
+            print(e, f'Название почты: {email}')
             continue
-        print(read_incoming_emails(HR_EMAILS[i], HR_PASSWORDS[i]))
+        # HR_EMAILS[index] - тоже самое, что email, но для единой структуры пишу так
+        read_incoming_emails(HR_EMAILS[index], HR_PASSWORDS[index], HR_IDS[index])
     sleep(10)
