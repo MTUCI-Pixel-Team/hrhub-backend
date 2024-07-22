9f86d7063f2ac77c9a559c32d05f7d66085c5ef1
prorok <prorok210c@gmail.com>
2024-07-22 04:51:06 +0300

    передалал создание/изменение людей

--- backend/user_app/urls.py
+++ backend/user_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('update-user/', views.ManageUserView.as_view(), name='update'),
     path('custom-user/', views.CustomUserView.as_view(), name='custom_user'),
     path('custom-user/<int:group_id>', views.ManageCustomUserView.as_view(), name='manage_custom_user'),
+    path('custom-user/get_uniq_users_from_messages/', views.GetUsernamesFromMessages.as_view(), name='get_uniq_users'),
 ]
